f1ae8f2464f7a9ddb2d46732ca82b86b3c9e60b2
Diego Castillo <diegocasmo@gmail.com>
2016-07-15 13:03:30 -0500

    Created 'about' page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # Article
   get '/articles', to: 'articles#index'
   get '/articles/:article_slug', to: 'articles#show'
+
+  # Static Pages
+  get '/about', to: 'static_pages#about'
 
   root to: 'articles#index'
 end
